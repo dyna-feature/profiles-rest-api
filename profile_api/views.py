e2b21db0bddd788aabeec716291bcd3188f6b2d9
fajar <fajarmochammad@gmail.com>
2024-04-03 14:43:17 +0700

    Add get API View, urls in project and urls in app

--- profile_api/views.py
+++ profile_api/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """Test API View"""
+
+    def get(self, request, format=None):
+        """Return a list of APIView features"""
+        an_apiview = [
+            'Uses HTTP Method to get, post, patch, put, delete',
+            'is similar to traditional view',
+            'Gives you the most control over your app logic',
+            'is mapped manually to Urls'
+        ]
+
+        return Response({'message': 'Hello!' , 'an_apiview': an_apiview})
