9a8c27b8cf753823de6af5ff484ca129ff8ee3c6
fajar <fajarmochammad@gmail.com>
2024-04-03 11:40:25 +0700

    add custom user profile model,manager and migrations

--- profile_api/models.py
+++ profile_api/models.py
@@ -1,3 +1,54 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser
+from django.contrib.auth.models import PermissionsMixin
+from django.contrib.auth.models import BaseUserManager
 
-# Create your models here.
+
+class UserProfileManager(BaseUserManager):
+    """Manager for user profiles"""
+
+    def create_user(self, email,name, password=None):
+        """Create a new user profile"""
+        if not email:
+            raise ValueError("user must have an email address")
+        email = self.normalize_email(email)
+        user = self.model(email=email, name=name)
+        user.set_password(password) #standar untuk hashing password di django
+        user.save(using=self._db) # standar django save procedure
+        return user
+
+    def create_super_user(self,email, name, password):
+        user = self.create_user(email,name,password)
+        user.is_superuser = True
+        user.is_staff = True
+        user.save(using=self._db)
+        return user     
+        
+
+class UserProfile(AbstractBaseUser, PermissionsMixin):
+    """Database model untuk user """
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+
+    objects = UserProfileManager()
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FILED = ['name']
+
+    def get_full_name(self):
+        """Retrieve full name of user"""
+        return self.name
+
+    def get_short_name(self):
+        """Retrieve shor name of user"""
+        return self.name 
+
+    def ___str___(self):
+        """Return string representation of our user"""
+        return self.email       
+    
+
+
+
